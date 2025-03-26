70466133c39d5d87b68ba95ac24d0b568e6e7547
DannyFink1 <114002047+DannyFink1@users.noreply.github.com>
2025-03-26 13:08:32 +0100

    added new card feature

--- app/cards/components/ui/card.tsx
+++ app/cards/components/ui/card.tsx
@@ -2,12 +2,14 @@ import React from 'react';
 
 interface CardProps {
   name: string;
+  title: string;
 }
 
-export default function Card({ name }: CardProps) {
+export default function Card({ name, title }: CardProps) {
   return (
     <div className="p-4 border rounded-lg shadow">
       <h2>{name}</h2>
+      <h1>{title}</h1>
     </div>
   );
 } 
--- app/cards/page.tsx
+++ app/cards/page.tsx
@@ -3,9 +3,9 @@ import Card from "./components/ui/card";
 
 export default function Cards() {
   return <div>
-    <Card name="John Doe" />
-    <Card name="Jane Doe" />
-    <Card name="John Smith" />
-    <Card name="Jane Smith" />  
+    <Card name="John Doe" title="Software Engineer" />
+    <Card name="Jane Doe" title="Software Engineer" />
+    <Card name="John Smith" title="Software Engineer" />
+    <Card name="Jane Smith" title="Software Engineer" />  
   </div>;
 }
